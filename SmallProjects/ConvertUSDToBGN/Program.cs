21631ef747c2c4e79cb5614de04e2af2bacb7921
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-01-15 18:17:35 +0200

    Add files via upload

---
+++ SmallProjects/ConvertUSDToBGN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertUSDToBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            Console.WriteLine($"{bgn:F2}");
+        }
+    }
+}
---
+++ SmallProjects/ProjectCreation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projectsCount = int.Parse(Console.ReadLine());
+            int timeProject = int.Parse($"{projectsCount * 3}");
+            Console.WriteLine($"The architect {name} will need {timeProject} hours to complete {projectsCount} project/s.");
+        }
+    }
+}
