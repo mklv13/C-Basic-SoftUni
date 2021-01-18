34ced2d193f00fa8fff544fcf0f3315f305699e0
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-01-18 11:45:25 +0200

    Add files via upload

---
+++ ConditionalStatements/SleepyCat/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SleepyCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int holidays = int.Parse(Console.ReadLine());
+            int minutesNorme = 30000;
+            int year = 365;
+            int workDays = year - holidays;
+            int minutesPlayWD = workDays * 63;
+            int minutesPlayHD = holidays * 127;
+            int totalMinutes = minutesPlayWD + minutesPlayHD;
+            if (totalMinutes > minutesNorme)
+            {
+                Console.WriteLine($"Tom will run away");
+                double result = totalMinutes - minutesNorme;
+                double minutes = result % 60;
+                double hour = Math.Floor(result / 60);
+                Console.WriteLine($"{hour} hours and {minutes} minutes more for play");
+            }
+            else
+            {
+                Console.WriteLine($"Tom sleeps well");
+                double result = minutesNorme - totalMinutes;
+                double minutes = result % 60;
+                double hours = Math.Floor(result / 60);
+                Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+            }
+        }
+    }
+}
