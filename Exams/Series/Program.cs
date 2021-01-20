c4624e2835d4fc85f23386f1b85b0651f64fc76e
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-01-20 20:33:33 +0200

    Add files via upload

---
+++ Exams/Series/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            double costs = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string serie = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                if (serie == "Thrones")
+                {
+                    price = 0.5 * price;
+                    costs += price;
+                }
+                else if (serie == "Lucifer")
+                {
+                    price = 0.6 * price;
+                    costs += price;
+                }
+                else if (serie == "Protector")
+                {
+                    price = 0.7 * price;
+                    costs += price;
+                }
+                else if (serie == "TotalDrama")
+                {
+                    price = 0.8 * price;
+                    costs += price;
+                }
+                else if (serie == "Area")
+                {
+                    price = 0.9 * price;
+                    costs += price;
+                }
+                else
+                {
+                    costs += price;
+                }
+
+            }
+            if (budget < costs)
+            {
+
+                Console.WriteLine($"You need {costs - budget:f2} lv. more to buy the series!");
+
+            }
+            if (budget >= costs)
+            {
+                Console.WriteLine($"You bought all the series and left with {budget - costs:f2} lv.");
+            }
+        }
+    }
+}
---
+++ Exams/TouristShop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TouristShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int counter = 0;
+            double totalPrice = 0;
+            for (int i = 0; i < budget; i++)
+            {
+                string product = Console.ReadLine();
+                if (product == "Stop")
+                {
+                    Console.WriteLine($"You bought {counter} products for {totalPrice:f2} leva.");
+                    break;
+                }
+                double price = double.Parse(Console.ReadLine());
+                if (price > budget)
+                {
+                    Console.WriteLine($"You don't have enough money!");
+                    Console.WriteLine($"You need {price - budget:f2} leva!");
+                    break;
+                }
+                counter++;
+                if (counter % 3 == 0)
+                {
+                    price *= 0.5;
+                }
+
+                totalPrice += price;
+                budget -= price;
+            }
+        }
+    }
+}
