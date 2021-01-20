1ceacb703c490a19aea7bc52dfc28cf764c18dca
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-01-20 20:35:22 +0200

    Add files via upload

---
+++ Exams/EasterShop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EasterShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            string input = "";
+            int soldEggs = 0;
+
+            while (input != "Close")
+            {
+                input = Console.ReadLine();
+                if (input == "Close")
+                {
+                    Console.WriteLine($"Store is closed!");
+                    Console.WriteLine($"{soldEggs} eggs sold.");
+                    break;
+                }
+                if (input == "Fill")
+                {
+
+                    int fillEggs = int.Parse(Console.ReadLine());
+                    quantity += fillEggs;
+                }
+                if (input == "Buy")
+                {
+
+                    int buyEggs = int.Parse(Console.ReadLine());
+                    if (buyEggs > quantity)
+                    {
+                        Console.WriteLine($"Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {quantity}.");
+                        break;
+                    }
+                    else
+                    {
+                        quantity -= buyEggs;
+                        soldEggs += buyEggs;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exams/FilmPremiere/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FilmPremiere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string film = Console.ReadLine();
+            string package = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double price = 0.0;
+            if (film == "John Wick")
+            {
+                if (package == "Drink") { price = 12.0 * quantity; }
+                else if (package == "Popcorn") { price = 15.00 * quantity; }
+                else { price = 19.00 * quantity; }
+            }
+            else if (film == "Star Wars")
+            {
+                if (package == "Drink") { price = 18.0 * quantity; }
+                else if (package == "Popcorn") { price = 25.00 * quantity; }
+                else { price = 30.00 * quantity; }
+            }
+            else
+            {
+                if (package == "Drink") { price = 9.0 * quantity; }
+                else if (package == "Popcorn") { price = 11.00 * quantity; }
+                else { price = 14.00 * quantity; }
+            }
+
+            if (film == "Star Wars" && quantity >= 4) { price = price * 0.70; }
+            if (film == "Jumanji" && quantity == 2) { price = price * 0.85; }
+            Console.WriteLine($"Your bill is {price:f2} leva.");
+        }
+    }
+}
