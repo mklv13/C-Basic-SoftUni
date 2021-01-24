42a93dfc83d20dd7dc76e5ce2d4d54bb27ce399c
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-01-24 13:19:04 +0200

    Add files via upload

---
+++ DataTypesAndVariablesExercise/PokeMon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startPower = int.Parse(Console.ReadLine()); 
+            int distTargets = int.Parse(Console.ReadLine());
+            int minPower = int.Parse(Console.ReadLine());
+            double originalValue = startPower * 0.50;
+
+            int count = 0;
+            while (startPower >= distTargets)
+            {
+                if (startPower == originalValue && minPower > 0)
+                {
+                    startPower /= minPower;
+                    minPower = distTargets;
+                    if (startPower < distTargets)
+                    {
+                        break;
+                    }
+                }
+                startPower -= distTargets;
+                count++;
+            }
+
+            Console.WriteLine(startPower);
+            Console.WriteLine(count);
+
+        }
+    }
+}
