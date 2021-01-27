9ffa1504557b0c1fb13edf2881f91d99251e769e
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-01-27 11:30:23 +0200

    Add files via upload

---
+++ Lists/SumReversedNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SumReversedNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                char[] arr = numbers[i].ToCharArray();
+                Array.Reverse(arr);
+                string number = new string(arr);
+                int num = int.Parse(number);
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
