a5f961aebed470b6571fa00bc4dc96dace1bb4d0
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-02-06 19:31:12 +0200

    Add files via upload

---
+++ Loops/HalsSumElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HalsSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+
+                if (number > max)
+                {
+                    max = number;
+                }
+
+            }
+            int sumWithoutMaxNumber = sum - max;
+            if (max == sumWithoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                int diff = Math.Abs(max - sumWithoutMaxNumber);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
