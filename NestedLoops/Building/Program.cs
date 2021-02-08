760d49a7ab9c3a32b737de8365e9594ce2bcdc4d
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-02-08 17:09:18 +0200

    Add files via upload

---
+++ NestedLoops/Building/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countFloor = int.Parse(Console.ReadLine());
+            int countChambers = int.Parse(Console.ReadLine());
+
+            for (int i = countFloor; i >= 1; i--)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < countChambers; j++)
+                {
+
+                    if (countFloor == 1 || i == countFloor)
+                    {
+                        Console.Write($"L{i}{j} ");
+
+                    }
+
+                    else if (i % 2 != 0)
+                    {
+                        Console.Write($"A{i}{j} ");
+
+                    }
+
+                    else if (i % 2 == 0)
+                    {
+                        Console.Write($"O{i}{j} ");
+
+                    }
+                }
+            }
+        }
+    }
+}
