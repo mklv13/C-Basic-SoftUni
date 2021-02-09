1a0b10db13263d0abe2bcbafba889ea9fa89f05d
mklv13 <56311144+mklv13@users.noreply.github.com>
2021-02-09 21:51:43 +0200

    Add files via upload

---
+++ NestedLoops/MultiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    int multiSum = i * j;
+                    Console.WriteLine($"{i} * {j} = {multiSum}");
+                }
+            }
+        }
+    }
+}
